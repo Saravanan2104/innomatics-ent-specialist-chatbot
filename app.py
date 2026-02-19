67b54c1a9ebf970a252cba3da696fb3f9033a9a0
Saravanan <147280058+Saravanan2104@users.noreply.github.com>
2026-02-19 17:41:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+import streamlit as st
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+
+load_dotenv()
+
+os.environ['GOOGLE_API_KEY'] = os.getenv('gemini_key')
+
+# Initialize the client once in session_state
+if 'client' not in st.session_state:
+    st.session_state.client = genai.Client()
+
+client = st.session_state.client
+
+system_prompt = """You are an ENT specialist.  provide solutions withing 2 or 3 lines.
+if user asks other than ENT related doubts respectly say visit other doctor or may i refer based on your illness"""
+
+st.title('ENT SPECIALIST Chatbot')
+st.write('This model is an ENT specialist. Ask your doubts related to Ear, Nose, and Throat.')
+
+
+# initialize chat session only once
+if 'chat_session' not in st.session_state:
+    st.session_state.chat_session = client.chats.create(
+        model = 'gemini-2.5-flash',
+        config = types.GenerateContentConfig(
+            system_instruction = system_prompt
+        )
+    )
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+# Display the past messages
+for role,text in st.session_state.messages:
+    if role == 'user':
+        st.markdown(f"**YOU:** {text}")
+    else:
+        st.markdown(f"**ENT SPECIALIST:** {text}")
+
+user_input = st.chat_input('Type you message here...')
+
+if user_input:
+    st.session_state.messages.append(('user',user_input))
+
+    chat = st.session_state.chat_session
+    bot_reply = chat.send_message(user_input).text
+
+    st.session_state.messages.append(('assistant',bot_reply))
+
+    st.rerun()
